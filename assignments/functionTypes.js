3c3862fbfca65616c05acc6787fd629eeb0a2d97
Kabir <kabir4691@gmail.com>
2019-12-17 23:06:45 +0530

    complete assigments

--- assignments/functionTypes.js
+++ assignments/functionTypes.js
@@ -43,11 +43,53 @@ convertToString(21); // "21"
  * @return {number}
  */
 
+//Function Decleration
+function addOne(n) {
+	return n + 1;
+}
+
+// Function Expression
+let addOne1 = function(n) {
+	return n + 1;
+};
+
+// Arrow Function
+let addOne2 = n => n + 1;
+
+// Arrow Function With Curly Bracket
+let addOne3 = n => {
+	return n + 1;
+};
+
+// Function Invocation
+addOne(21);
+
 /**
  * Subtracts one from a given number.
  * @param {number} n
  * @return {number}
  */
+
+//Function Decleration
+function subtractOne(n) {
+	return n - 1;
+}
+
+// Function Expression
+let subtractOne1 = function(n) {
+	return n - 1;
+};
+
+// Arrow Function
+let subtractOne2 = n => n - 1;
+
+// Arrow Function With Curly Bracket
+let subtractOne3 = n => {
+	return n - 1;
+};
+
+// Function Invocation
+subtractOne(21);
 
 /**
  * Adds two numbers.
@@ -56,12 +98,54 @@ convertToString(21); // "21"
  * @return {number} the sum
  */
 
+//Function Decleration
+function add(a, b) {
+	return a + b;
+}
+
+// Function Expression
+let add1 = function(a, b) {
+	return a + b;
+};
+
+// Arrow Function
+let add2 = (a, b) => a + b;
+
+// Arrow Function With Curly Bracket
+let add3 = (a, b) => {
+	return a + b;
+};
+
+// Function Invocation
+add(1, 2);
+
 /**
  * Subtracts the second number from the first.
  * @param {number} x
  * @param {number} y
  * @return {number} the difference
  */
+
+//Function Decleration
+function subtract(a, b) {
+	return a - b;
+}
+
+// Function Expression
+let subtract1 = function(a, b) {
+	return a - b;
+};
+
+// Arrow Function
+let subtract2 = (a, b) => a - b;
+
+// Arrow Function With Curly Bracket
+let subtract3 = (a, b) => {
+	return a - b;
+};
+
+// Function Invocation
+subtract(9, 4);
 
 /**
  * Multiplies two numbers.
@@ -70,6 +154,27 @@ convertToString(21); // "21"
  * @return {number} the product
  */
 
+//Function Decleration
+function multiply(a, b) {
+	return a * b;
+}
+
+// Function Expression
+let multiply1 = function(a, b) {
+	return a * b;
+};
+
+// Arrow Function
+let multiply2 = (a, b) => a * b;
+
+// Arrow Function With Curly Bracket
+let multiply3 = (a, b) => {
+	return a * b;
+};
+
+// Function Invocation
+multiply(9, 4);
+
 /**
  * Divides the first number by the second.
  * @param {number} x
@@ -77,11 +182,53 @@ convertToString(21); // "21"
  * @return {number} the quotient
  */
 
+//Function Decleration
+function divide(a, b) {
+	return a / b;
+}
+
+// Function Expression
+let divide1 = function(a, b) {
+	return a / b;
+};
+
+// Arrow Function
+let divide2 = (a, b) => a / b;
+
+// Arrow Function With Curly Bracket
+let divide3 = (a, b) => {
+	return a / b;
+};
+
+// Function Invocation
+divide(9, 4);
+
 /**
  * Multiplies a number by itself.
  * @param {number} x, number to be squared
  * @return {number} squared
  */
+
+//Function Decleration
+function multiplyByItself(a) {
+	return a * a;
+}
+
+// Function Expression
+let multiplyByItself1 = function(a) {
+	return a * a;
+};
+
+// Arrow Function
+let multiplyByItself2 = a => a * a;
+
+// Arrow Function With Curly Bracket
+let multiplyByItself3 = a => {
+	return a * a;
+};
+
+// Function Invocation
+multiplyByItself(9);
 
 /**
  * Performs a mathematical operation on two numbers.
@@ -92,12 +239,120 @@ convertToString(21); // "21"
  * @return {number} the result
  */
 
+//Function Decleration
+function doMath(operation, x, y) {
+	switch (operation) {
+		case "add":
+			console.log(`${x} + ${y} = ${x + y}`);
+			break;
+		case "subtract":
+			console.log(`${x} - ${y} = ${x - y}`);
+			break;
+		case "multiply":
+			console.log(`${x} * ${y} = ${x * y}`);
+			break;
+		case "divide":
+			console.log(`${x} / ${y} = ${x / y}`);
+			break;
+		default:
+			console.log("Invalid operation");
+			break;
+	}
+}
+
+// Function Expression
+let doMath1 = function(operation, x, y) {
+	switch (operation) {
+		case "add":
+			console.log(`${x} + ${y} = ${x + y}`);
+			break;
+		case "subtract":
+			console.log(`${x} - ${y} = ${x - y}`);
+			break;
+		case "multiply":
+			console.log(`${x} * ${y} = ${x * y}`);
+			break;
+		case "divide":
+			console.log(`${x} / ${y} = ${x / y}`);
+			break;
+		default:
+			console.log("Invalid operation");
+			break;
+	}
+};
+
+// Arrow Function
+let doMath2 = (operation, x, y) => {
+	switch (operation) {
+		case "add":
+			console.log(`${x} + ${y} = ${x + y}`);
+			break;
+		case "subtract":
+			console.log(`${x} - ${y} = ${x - y}`);
+			break;
+		case "multiply":
+			console.log(`${x} * ${y} = ${x * y}`);
+			break;
+		case "divide":
+			console.log(`${x} / ${y} = ${x / y}`);
+			break;
+		default:
+			console.log("Invalid operation");
+			break;
+	}
+};
+
+// Arrow Function With Curly Bracket
+let doMath3 = (operation, x, y) => {
+	switch (operation) {
+		case "add":
+			console.log(`${x} + ${y} = ${x + y}`);
+			break;
+		case "subtract":
+			console.log(`${x} - ${y} = ${x - y}`);
+			break;
+		case "multiply":
+			console.log(`${x} * ${y} = ${x * y}`);
+			break;
+		case "divide":
+			console.log(`${x} / ${y} = ${x / y}`);
+			break;
+		default:
+			console.log("Invalid operation");
+			break;
+	}
+};
+
+// Function Invocation
+doMath("add", 3, 5);
+
 /**
  * Returns true if `a` is greater than `b`.
  * @param {number} a
  * @param {number} b
  * @return {boolean} `a` is larger than `b`
  */
+
+//Function Decleration
+function isGreaterThan(a, b) {
+	return a > b;
+}
+
+// Function Expression
+let isGreaterThan1 = function(a, b) {
+	return a > b;
+};
+
+// Arrow Function
+let isGreaterThan2 = (a, b) => a > b;
+
+// Arrow Function With Curly Bracket
+let isGreaterThan3 = (a, b) => {
+	return a > b;
+};
+
+// Function Invocation
+isGreaterThan(9, 4);
 
 /**
  * Returns true if `a` is less than `b`.
@@ -106,12 +361,54 @@ convertToString(21); // "21"
  * @return {boolean} `a` is smaller than `b`
  */
 
+//Function Decleration
+function isLesserThan(a, b) {
+	return a < b;
+}
+
+// Function Expression
+let isLesserThan1 = function(a, b) {
+	return a < b;
+};
+
+// Arrow Function
+let isLesserThan2 = (a, b) => a < b;
+
+// Arrow Function With Curly Bracket
+let isLesserThan3 = (a, b) => {
+	return a < b;
+};
+
+// Function Invocation
+isLesserThan(9, 4);
+
 /**
  * Returns true if `a` and `b` are equal.
  * @param {number} a
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
+
+//Function Decleration
+function isEqualTo(a, b) {
+	return a == b;
+}
+
+// Function Expression
+let isEqualTo1 = function(a, b) {
+	return a == b;
+};
+
+// Arrow Function
+let isEqualTo2 = (a, b) => a == b;
+
+// Arrow Function With Curly Bracket
+let isEqualTo3 = (a, b) => {
+	return a == b;
+};
+
+// Function Invocation
+isEqualTo(9, 4);
 
 /**
  * Returns the smallest value of two numbers.
@@ -120,6 +417,27 @@ convertToString(21); // "21"
  * @return {number} the smallest number
  */
 
+//Function Decleration
+function min(a, b) {
+	return Math.min(a, b);
+}
+
+// Function Expression
+let min1 = function(a, b) {
+	return Math.min(a, b);
+};
+
+// Arrow Function
+let min2 = (a, b) => Math.min(a, b);
+
+// Arrow Function With Curly Bracket
+let min3 = (a, b) => {
+	return Math.min(a, b);
+};
+
+// Function Invocation
+min(9, 4);
+
 /**
  * Returns the largest value of two numbers.
  * @param {number} x
@@ -127,17 +445,80 @@ convertToString(21); // "21"
  * @return {number} the largest number
  */
 
+//Function Decleration
+function max(a, b) {
+	return Math.max(a, b);
+}
+
+// Function Expression
+let max1 = function(a, b) {
+	return Math.max(a, b);
+};
+
+// Arrow Function
+let max2 = (a, b) => Math.max(a, b);
+
+// Arrow Function With Curly Bracket
+let max3 = (a, b) => {
+	return Math.max(a, b);
+};
+
+// Function Invocation
+max(9, 4);
+
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
 
+//Function Decleration
+function isEven(a) {
+	return a % 2 == 0;
+}
+
+// Function Expression
+let isEven1 = function(a) {
+	return a % 2 == 0;
+};
+
+// Arrow Function
+let isEven2 = a => a % 2 == 0;
+
+// Arrow Function With Curly Bracket
+let isEven3 = a => {
+	return a % 2 == 0;
+};
+
+// Function Invocation
+isEven(9);
+
 /**
  * Returns true if `n` is odd.
  * @param {number} n
  * @return {boolean} the number is odd
  */
+
+ //Function Decleration
+function isOdd(a) {
+	return !isEven(a);
+}
+
+// Function Expression
+let isOdd1 = function(a) {
+	return !isEven(a);
+};
+
+// Arrow Function
+let isOdd2 = a => !isEven(a);
+
+// Arrow Function With Curly Bracket
+let isOdd3 = a => {
+	return !isEven(a);
+};
+
+// Function Invocation
+isOdd(9);
 
 /**
  * Returns a letter grade.
@@ -151,9 +532,97 @@ convertToString(21); // "21"
  * @return {string} the score represented as a letter grade
  */
 
+//Function Decleration
+function getGrade(score, total) {
+  let percent = score / total;
+  if (percent >= 90) {
+		return "A";
+  } else if (percent >= 80) {
+		return "B";
+  } else if (percent >= 70) {
+		return "C";
+  } else if (percent >= 60) {
+		return "D";
+  } else {
+		return "F";
+  }
+}
+
+// Function Expression
+let getGrade1 = function(score, total) {
+	let percent = score / total;
+	if (percent >= 90) {
+		return "A";
+	} else if (percent >= 80) {
+		return "B";
+	} else if (percent >= 70) {
+		return "C";
+	} else if (percent >= 60) {
+		return "D";
+	} else {
+		return "F";
+	}
+};
+
+// Arrow Function
+let getGrade2 = (score, total) => {
+	let percent = score / total;
+	if (percent >= 90) {
+		return "A";
+	} else if (percent >= 80) {
+		return "B";
+	} else if (percent >= 70) {
+		return "C";
+	} else if (percent >= 60) {
+		return "D";
+	} else {
+		return "F";
+	}
+};
+
+// Arrow Function With Curly Bracket
+let getGrade3 = getGrade1 => {
+	let percent = score / total;
+	if (percent >= 90) {
+		return "A";
+	} else if (percent >= 80) {
+		return "B";
+	} else if (percent >= 70) {
+		return "C";
+	} else if (percent >= 60) {
+		return "D";
+	} else {
+		return "F";
+	}
+};
+
+// Function Invocation
+getGrade(3, 5);
+
 /**
  * Joins two strings with a space.
  * @param {string} word1
  * @param {string} word2
  * @return {string} joined the words joined with a space
  */
+
+ //Function Decleration
+function joinWithSpace(a, b) {
+  return a + " " + b;
+}
+
+// Function Expression
+let joinWithSpace1 = function(a, b) {
+	return a + " " + b;
+};
+
+// Arrow Function
+let joinWithSpace2 = (a, b) => a + " " + b;
+
+// Arrow Function With Curly Bracket
+let joinWithSpace3 = (a, b) => {
+	return a + " " + b;
+};
+
+// Function Invocation
+joinWithSpace(9, 4);
--- assignments/functions.md
+++ assignments/functions.md
@@ -5,8 +5,11 @@
   * [ ] Add an additional argument to the function that takes the conversion rate of human to dog years.
 
 ```js
-// your code goes here
+function calculateDogAge(age) {
+	console.log(`Your doggie is ${age * 7} years old in dog years!`);
+}
 ```
+
 2. 🎖Write a function named calculateSupply that:
   * [ ] takes 2 arguments: age, amount per day.
   * [ ] calculates the amount consumed for rest of the life (based on a constant max age).
@@ -14,7 +17,12 @@
   * [ ] Accept floating point values for amount per day, and round the result to a round number.
 
 ```js
-// your code goes here
+function calculateSupply(age, amount) {
+	const MAX_AGE = 80;
+	let remainingAge = MAX_AGE - age;
+	let requiredAmount = Math.round(remainingAge * amount);
+	console.log(`You will need ${requiredAmount} to last you until the ripe old age of ${MAX_AGE}`);
+}
 ```
 3. 🎖Create a function called celsiusToFahrenheit:
   * [ ] Store a celsius temperature into a variable.
@@ -24,28 +32,41 @@
   * [ ] Convert it to celsius and output "NN°F is NN°C."
 
 ```js
-// your code goes here
+function celsiusToFahrenheit(celsius) {
+	let fahrenheit = (celsius * 9) / 5 + 32;
+	console.log(`${celsius}°C is ${fahrenheit}°F`);
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+	let celsius = ((fahrenheit - 32) * 5) / 9;
+	console.log(`${fahrenheit}°F is ${celsius}°C`);
+}
 ```
+
 4. 🎖The function below returns true if the parameter age is greater than 18. Otherwise it asks for a confirmation and returns its result:
 
 ```js
 function checkAge(age) {
-  if (age > 18) {
-    return true;
-  } else {
-    // ...
-    return confirm("Did parents allow you?");
-  }
+	if (age > 18) {
+		return true;
+	} else {
+		// ...
+		return confirm("Did parents allow you?");
+	}
 }
 ```
   4.1 🎖Convert the above function using ternary operator.
   ```js
-  // your code goes here
+  function checkAge(age) {
+   	return age > 18 ? true : confirm("Did parents allow you?");
+  }
   ```
 
   4.2 🎖Convert the above function using `||` operator.
   ```js
-  // your code goes here
+  function checkAge(age) {
+   	return age > 18 || confirm("Did parents allow you?");
+  }
   ```
 Will the function work differently if else is removed like below?
 
@@ -60,6 +81,7 @@ function checkAge(age) {
 ```
 Is there any difference in the behavior of these two variants? If there is what is that?
 
+No
 
 5. 🎖 Write a function pow(x,n) that returns x in power n.
 
@@ -67,37 +89,100 @@ Is there any difference in the behavior of these two variants? If there is what 
   * [ ] In this task the function should support only natural values of n: integers greater then 1.
 
 ```js
-// Your code goes here
+function pow(x, n) {
+	if (x < 1 || n < 1) {
+		console.log("The number below 1 is not allowed");
+		return;
+	}
+	console.log(x ** n);
+}
 
-// After writing code uncomment to check the answer.
-// pow(3, 2); // 9
-// pow(3, 3); // 27
-// pow(1, 100); // 1
-// pow(-31, 2); // "The number below 1 is not allowed"
+pow(3, 2); // 9
+pow(3, 3); // 27
+pow(1, 100); // 1
+pow(-31, 2); // "The number below 1 is not allowed"
+```
 
 6. 🎖Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n. Return the result accordingly.
 
 ```js
-// your code goes here
+function product(number) {
+	if (number == 0) {
+		return 1;
+	}
+	return number * product(number - 1);
+}
+
+function sum(number) {
+	if (number == 0) {
+		return 0;
+	}
+	return number + sum(number - 1);
+}
+
+let numString = prompt("Enter number");
+let choiceString = prompt("Enter 1 for sum, 2 for product");
+
+let num = parseInt(numString);
+let choice = parseInt(choiceString);
+
+switch (choice) {
+	case 1:
+		alert(sum(num));
+		break;
+	case 2:
+		alert(product(num));
+		break;
+	default:
+		alert("Invalid choice");
+		break;
+}
+
 ```
 6. 🎖Write a program that asks the user for a number n using prompt and prints the sum of the numbers 1 to n
 
 ```js
-// your code goes here
+function sum(number) {
+	if (number == 0) {
+		return 0;
+	}
+	return number + sum(number - 1);
+}
+
+let numString = prompt("Enter number");
+let num = parseInt(numString);
+
+alert(sum(num));
 ```
 7. 🎖Modify the previous program such that only multiples of 5 or 7 are considered in the sum, e.g. n = 20 (5,7,10,14,15,20) 71
 
 ```js
-// your code goes here
+function sum(number) {
+	if (number == 0) {
+		return 0;
+  }
+  // If number cannot be divided by neither 5 nor 7
+  if (number % 5 && number % 7) {
+    return sum(number - 1);
+  } 
+	return number + sum(number - 1);
+}
+
+let numString = prompt("Enter number");
+let num = parseInt(numString);
+
+alert(sum(num));
 ```
 
 8. 🎖Write a function `min` that takes two arguments and returns their minimum.
 
 ```js
-// Your code here.
+function min(a, b) {
+  return Math.min(a, b);
+}
 
 console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
-```
+``````
